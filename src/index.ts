8f0c7b4437d24c867762dc8d25a6134072fa90b3
Sukhov <yevhenii.sukhov@hygge.software>
2022-05-19 17:31:12 +0300

    Implemented language detector

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 declare var FroalaEditor: any;
 import * as shiki from 'shiki';
-const CDN = 'https://unpkg.com/shiki/';
-
+import { detect, Lang, LANG } from 'program-language-detector';
 import * as svg from '../icon/ico.svg';
 
 const { error } = console;
+type LangKey = keyof Lang;
+const CDN = 'https://unpkg.com/shiki/';
+
+const LangTypes = {
+  ...LANG,
+  [LANG['C++']]: 'c',
+};
 
 class IntegrationModel {
   private defaultLangList: shiki.Lang[];
+  private loadedLanguages: shiki.Lang[];
   private highlighter: shiki.Highlighter | null;
   private defaultTheme: StringLiteralUnion<shiki.Theme>;
 
   constructor() {
     this.highlighter = null;
-    this.defaultLangList = ['js', 'ruby'];
+    this.defaultLangList = ['js', 'ruby', 'c', 'css', 'html', 'python'];
+    this.loadedLanguages = [];
     this.defaultTheme = 'min-light';
     shiki.setCDN(CDN);
     this.createHighlighter();
@@ -25,32 +33,63 @@ class IntegrationModel {
   ): Promise<void> {
     try {
       this.highlighter = await shiki.getHighlighter({ langs, theme });
+      this.loadedLanguages = langs;
     } catch (e: any) {
       error(`Can't create highlighter object - ${e}`);
     }
   }
 
-  public codeToHtml(source: string, lang: StringLiteralUnion<shiki.Lang>): string {
-    return this.highlighter?.codeToHtml(source, { lang }) || source;
+  public async codeToHtml(source: string, lang: shiki.Lang): Promise<string> {
+    if (!this.loadedLanguages.includes(lang)) {
+      await this.highlighter?.loadLanguage(lang);
+      this.loadedLanguages.push(lang);
+    }
+    return `${this.highlighter?.codeToHtml(source, { lang })}<p></p>` || source;
+  }
+
+  public cleanEmpty(froalaEl: HTMLDivElement): void {
+    const shikiEls = froalaEl.querySelectorAll('.shiki');
+
+    for (const el of shikiEls) {
+      if (!el.textContent?.trim().replace(/[\u200B-\u200D\uFEFF]/g, '').length) {
+        el.remove();
+      } else {
+        const codeEls = el.querySelectorAll('code');
+
+        for (const codeEl of codeEls) {
+          if (!codeEl.textContent?.trim().replace(/[\u200B-\u200D\uFEFF]/g, '').length) {
+            codeEl.remove();
+          }
+        }
+      }
+    }
   }
 }
 
 (function (FE) {
   const integrationModel = new IntegrationModel();
 
+
   FE.PLUGINS.syntaxHighlighter = function (editor: any) {
     function _init() {
-      console.log('plugin initialized');
+
     }
 
-    function highlight() {
-      const selection = editor.selection.get();
+    async function highlight(): Promise<void> {
       try {
-        let html = integrationModel.codeToHtml(selection.toString(), 'ruby');
+        const selection = editor.selection.get().toString();
+        const detectedLang: LangKey = detect(selection) as LangKey;
+        const lang = LangTypes[detectedLang as LangKey].toLowerCase();
+        if (lang === LangTypes.Unknown.toLowerCase()) {
+          return;
+        }
+        let html = await integrationModel.codeToHtml(selection, lang as shiki.Lang);
         html = editor.clean.html(html);
         editor.html.insert(html);
         editor.html.unwrap();
         editor.selection.restore();
+        const [froalaEl] = editor.$el.get();
+        integrationModel.cleanEmpty(froalaEl);
       } catch (e: any) {
         error(e);
       }
@@ -59,14 +98,15 @@ class IntegrationModel {
     return { highlight, _init };
   };
 
-  FroalaEditor.DefineIconTemplate('highlightCodePlugin', '[SVG]');
-  FroalaEditor.DefineIcon('highlightCode', { SVG: svg, template: 'highlightCodePlugin' });
+  FE.DefineIconTemplate('highlightCodePlugin', '[SVG]');
+  FE.DefineIcon('highlightCode', { SVG: svg, template: 'highlightCodePlugin' });
   FE.RegisterCommand('highlightCode', {
     title: 'Highlight your code',
     undo: true,
     focus: false,
-    callback: function () {
+    callback: function() {
       this.syntaxHighlighter.highlight();
     },
   });
+  FE.RegisterShortcut(70, 'highlightCode', 'F', 'F', false);
 })(FroalaEditor);
